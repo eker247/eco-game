21bcf3252f53522fbc1b0b93368798aead8abc07
eker247 <eker247@wp.pl>
2020-02-23 20:27:29 +0100

    removed stage.enum

--- src/logic/stages/stage-course.ts
+++
@@ -1,6 +0,0 @@
-import { StagesEnum } from "./stages.enum";
-
-export class StageCourse {
-  stage: StagesEnum = StagesEnum.BUYING_STATION;
-  
-}
--- src/logic/stages/stages.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum StagesEnum {
-  BUYING_STATION = 'Buying station',
-  BUYING_RESOURCE = 'Buying resource',
-  NOT_IMPLEMENTED = 'Not implemented',
-}
-
-// export type SingleStage {
-//   [key of StagesEnum]: 'string'
-// }

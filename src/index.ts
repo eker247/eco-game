205f858f6aebee239e23c12a6cd19451b195aeb5
eker247 <eker247@wp.pl>
2020-03-01 12:00:53 +0100

    added setting service

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { ResourceEnum } from './logic/stages/resource/resource.enum';
 import { ResourceService } from './logic/stages/resource/resource.service';
 import { ResourceStage } from './logic/stages/resource/resource.stage';
 import { Station, StationService, StationStage } from './logic/stages/station';
+import { SettingService } from './logic/setting.service';
 
 export {
   House,
@@ -17,6 +18,7 @@ export {
   ResourceEnum,
   ResourceService,
   ResourceStage,
+  SettingService,
   Station,
   StationService,
   StationStage,

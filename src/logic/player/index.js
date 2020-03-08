8b1c05fc3aea4c013eb91af1765779b105c13be5
eker247 <eker247@wp.pl>
2020-03-08 20:25:19 +0100

    StationStage.removePlayer

--- src/logic/player/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var player_1 = require("./player");
-exports.Player = player_1.Player;
-var player_service_1 = require("./player.service");
-exports.PlayerService = player_service_1.PlayerService;
